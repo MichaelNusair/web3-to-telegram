8b87af526fdef41d5a8b1fe5aa240125ea03ccbc
Michael Nusair <michaelnusair@gotolstoy.com>
2025-08-24 22:35:37 +0300

    fix

--- src/lambdas/index.ts
+++ src/lambdas/index.ts
@@ -54,10 +54,7 @@ const WATCH_LIST = [
   },
 ];
 
-const ALERT_THRESHOLD = ethers.parseUnits(
-  getEnvVar("ALERT_THRESHOLD_TOKENS"),
-  18
-);
+const ALERT_THRESHOLD = ethers.parseUnits("50000", 18);
 
 /* ─────────── Chain interfaces ────────── */
 
